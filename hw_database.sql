66e12e7add20f03d4690f096978eae22613a30ee
老周部落 <31075951+laozhoubuluo@users.noreply.github.com>
2019-03-12 10:17:01 +0800

    目标数据库架构

--- hw_database.sql
+++ hw_database.sql
@@ -21,12 +21,12 @@ SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
 
 CREATE TABLE IF NOT EXISTS `hwList` (
   `hID` int(11) NOT NULL auto_increment,
-  `hwTitle` varchar(100) collate utf8_bin NOT NULL,
-  `hwO` varchar(20) collate utf8_bin NOT NULL,
-  `email` varchar(60) collate utf8_bin NOT NULL,
-  `classID` varchar(12) collate utf8_bin NOT NULL,
-  `remark` varchar(255) collate utf8_bin NOT NULL,
-  `passwd` varchar(20) collate utf8_bin NOT NULL,
+  `hwTitle` varchar(100) collate utf8mb4_unicode_ci NOT NULL,
+  `hwO` varchar(20) collate utf8mb4_unicode_ci NOT NULL,
+  `email` varchar(60) collate utf8mb4_unicode_ci NOT NULL,
+  `classID` varchar(12) collate utf8mb4_unicode_ci NOT NULL,
+  `remark` varchar(1000) collate utf8mb4_unicode_ci NOT NULL,
+  `passwd` varchar(20) collate utf8mb4_unicode_ci NOT NULL,
   `fromDT` date NOT NULL,
   `dueDT` date NOT NULL,
   `display` tinyint(4) NOT NULL default '1',
@@ -40,7 +40,7 @@ CREATE TABLE IF NOT EXISTS `hwList` (
   `uDT` timestamp NOT NULL default '0000-00-00 00:00:00' on update CURRENT_TIMESTAMP,
   `cDT` datetime NOT NULL,
   PRIMARY KEY  (`hID`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_bin AUTO_INCREMENT=7 ;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci AUTO_INCREMENT=1;
 
 --
 -- 资料表格式： `hwUpload`
@@ -49,21 +49,22 @@ CREATE TABLE IF NOT EXISTS `hwList` (
 CREATE TABLE IF NOT EXISTS `hwUpload` (
   `sn` int(11) NOT NULL auto_increment,
   `hID` int(11) NOT NULL,
-  `cname` varchar(20) collate utf8_bin NOT NULL,
-  `modPasswd` varchar(20) collate utf8_bin default NULL,
-  `fileName` varchar(100) collate utf8_bin NOT NULL,
-  `oFileName` varchar(100) collate utf8_bin NOT NULL,
-  `remark` varchar(200) collate utf8_bin NOT NULL,
+  `cid` varchar(20) collate utf8mb4_unicode_ci NOT NULL,
+  `cname` varchar(20) collate utf8mb4_unicode_ci NOT NULL,
+  `modPasswd` varchar(20) collate utf8mb4_unicode_ci default NULL,
+  `fileName` varchar(100) collate utf8mb4_unicode_ci NOT NULL,
+  `oFileName` varchar(100) collate utf8mb4_unicode_ci NOT NULL,
+  `remark` varchar(200) collate utf8mb4_unicode_ci NOT NULL,
   `size` int(11) NOT NULL,
-  `ext` varchar(10) collate utf8_bin NOT NULL,
+  `ext` varchar(10) collate utf8mb4_unicode_ci NOT NULL,
   `passed` tinyint(4) NOT NULL default '0',
-  `score` varchar(12) collate utf8_bin NOT NULL default '0',
+  `score` varchar(12) collate utf8mb4_unicode_ci NOT NULL default '0',
   `display` tinyint(4) NOT NULL default '0',
   `uDT` datetime NOT NULL,
   `cDT` datetime NOT NULL,
   PRIMARY KEY  (`sn`),
   KEY `hID` (`hID`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_bin AUTO_INCREMENT=9 ;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci AUTO_INCREMENT=1;
 
 --
 -- 资料表格式： `member`
@@ -90,7 +91,7 @@ CREATE TABLE IF NOT EXISTS `member` (
   `regDT` datetime NOT NULL COMMENT 'a﹐3a??e‥?a??a??e–“',
   `role` varchar(64) NOT NULL default '2',
   PRIMARY KEY  (`email`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci AUTO_INCREMENT=1;
 
 --
 -- 列出以下资料库的数据： `member`
@@ -106,26 +107,28 @@ INSERT INTO `member` (`email`, `usID`, `comID`, `cname`, `exp`, `shadow`, `passw
 --
 
 CREATE TABLE IF NOT EXISTS `reclogin` (
+  `lID` int(11) NOT NULL auto_increment,
   `recDT` datetime NOT NULL,
-  `email` varchar(60) collate utf8_bin NOT NULL,
-  `ip` varchar(40) collate utf8_bin NOT NULL,
-  `type` varchar(20) collate utf8_bin NOT NULL,
-  `desc` varchar(100) collate utf8_bin default NULL,
-  PRIMARY KEY  (`recDT`,`email`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+  `email` varchar(60) collate utf8mb4_unicode_ci NOT NULL,
+  `ip` varchar(40) collate utf8mb4_unicode_ci NOT NULL,
+  `type` varchar(20) collate utf8mb4_unicode_ci NOT NULL,
+  `desc` varchar(100) collate utf8mb4_unicode_ci default NULL,
+  PRIMARY KEY  (`lID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci AUTO_INCREMENT=1;
 
 --
 -- 资料表格式： `recmng`
 --
 
 CREATE TABLE IF NOT EXISTS `recmng` (
+  `mID` int(11) NOT NULL auto_increment,
   `recDT` datetime NOT NULL,
-  `email` varchar(60) collate utf8_bin NOT NULL,
-  `ip` varchar(40) collate utf8_bin NOT NULL,
-  `type` varchar(20) collate utf8_bin NOT NULL,
-  `desc` varchar(100) collate utf8_bin default NULL,
-  PRIMARY KEY  (`recDT`,`email`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+  `email` varchar(60) collate utf8mb4_unicode_ci NOT NULL,
+  `ip` varchar(40) collate utf8mb4_unicode_ci NOT NULL,
+  `type` varchar(20) collate utf8mb4_unicode_ci NOT NULL,
+  `desc` varchar(100) collate utf8mb4_unicode_ci default NULL,
+  PRIMARY KEY  (`mID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci AUTO_INCREMENT=1;
 
 --
 -- 列出以下资料库的数据： `recmng`
@@ -137,13 +140,14 @@ CREATE TABLE IF NOT EXISTS `recmng` (
 --
 
 CREATE TABLE IF NOT EXISTS `recuser` (
+  `uID` int(11) NOT NULL auto_increment,
   `recDT` datetime NOT NULL,
-  `email` varchar(60) collate utf8_bin NOT NULL,
-  `ip` varchar(40) collate utf8_bin NOT NULL,
-  `type` varchar(20) collate utf8_bin NOT NULL,
-  `desc` varchar(100) collate utf8_bin default NULL,
-  PRIMARY KEY  (`recDT`,`email`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+  `email` varchar(60) collate utf8mb4_unicode_ci NOT NULL,
+  `ip` varchar(40) collate utf8mb4_unicode_ci NOT NULL,
+  `type` varchar(20) collate utf8mb4_unicode_ci NOT NULL,
+  `desc` varchar(100) collate utf8mb4_unicode_ci default NULL,
+  PRIMARY KEY  (`uID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci AUTO_INCREMENT=1;
 
 
 -- 资料表限制 `hwUpload`
